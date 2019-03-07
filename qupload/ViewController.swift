ef5034dc456dbc726981bcd9c39329d8c7151d92
MLeo <lb52406@qq.com>
2019-03-07 15:39:43 +0800

    Initial Commit

---
+++ qupload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  qupload
+//
+//  Created by MLeo on 2019/3/7.
+//  Copyright © 2019年 iChochy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
