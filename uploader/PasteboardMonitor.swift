0e6c1add1f69a967f81e7e514a8c3c6a0ee0d7c9
MLeo <lb52406@qq.com>
2019-12-07 20:52:57 +0800

    monitor

--- uploader/PasteboardMonitor.swift
+++ uploader/PasteboardMonitor.swift
@@ -35,7 +35,8 @@ class PasteboardMonitor {
     
     /// 开启监控
     func open(){
-        timerSource.resume();
+        self.changeCount = -1
+        timerSource.resume()
     }
     
     func close(){
